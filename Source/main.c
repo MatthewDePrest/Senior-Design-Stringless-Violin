896b35c1c9e09c895273f42cc0736dabed244903
Oliver Rey Fortmann <ofortman@eceprog5.ecn.purdue.edu>
2025-09-16 09:14:30 -0400

    allData Test

--- Source/main.c
+++ Source/main.c
@@ -4,19 +4,20 @@
 
 int main(int argc, char const *argv[])
 {
-    allData* data;
-    data->pressures[0] = 10;
-    data->pressures[0] = 0;
-    data->pressures[0] = 6;
-    data->pressures[0] = 14;
-    data->positions[0] = 0.5;
-    data->positions[0] = 0.5;
-    data->positions[0] = 0.5;
-    data->positions[0] = 0.5;
-    data->bowSpeed = -6;
-    data->lowestFreq = 120;
+    allData data;
+    data.pressures[0] = 10;
+    data.pressures[1] = 0;
+    data.pressures[2] = 6;
+    data.pressures[3] = 14;
+    data.positions[0] = 0;
+    data.positions[1] = 0;
+    data.positions[2] = 0;
+    data.positions[3] = 0;
+    data.bowSpeed = -6;
+    data.lowestFreq = 120;
+    data.freq = 200;
 
-    calculatePitch(data);
-    printf("%.1f\n", data->freq);
+    calculatePitch(&data);
+    printf("%.1f\n", data.freq);
     return 0;
 }
